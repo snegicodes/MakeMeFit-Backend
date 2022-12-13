d009833d3908a108e8de056da3f177062fb5dbc5
snegicodes <snegicodesmain@gmail.com>
2022-12-14 03:06:22 +0530

    added cors

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const workoutRoutes = require("./routes/workouts");
 
 //Express app
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 mongoose.set("strictQuery", true);
 
