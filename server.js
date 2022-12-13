8438c837d24e1c476f8477aa3f7da771e5468cf6
snegicodes <snegicodesmain@gmail.com>
2022-12-14 02:43:34 +0530

    Base Commit

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+
+//Express app
+const app = express();
+
+mongoose.set("strictQuery", true);
+
+//Middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//Routes
+app.use("/api/workouts", workoutRoutes);
+
+//DB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //Listening to Request
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB and Listening to port", process.env.PORT);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
